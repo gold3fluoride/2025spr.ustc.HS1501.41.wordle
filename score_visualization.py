b2331758fd709ad5d4d81d181b28ce94574e5b19
gold3fluoride <wangxinzhe2008@163.com>
2025-05-04 16:47:11 +0800

    Add files via upload

---
+++ score_visualization.py
@@ -0,0 +1,48 @@
+import os
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+w1 = 7/7
+
+os.chdir('C:\\Users\\GU\\Desktop') 
+df = pd.read_excel('Wordle_cleaned.xlsx')
+#print(df)
+
+df['score'] = 7*w1* df['7 or more tries (X)'] +  (1*df['1 try'] + 2*df['2 tries'] + 3*df['3 tries'] + 4*df['4 tries'] + 5*df['5 tries'] + 6*df['6 tries'])
+
+
+scores = df['score'].values
+min_score, max_score = min(scores), max(scores)
+# max: nymph; min: train;parer
+for i in range(len(scores)):
+    if scores[i]<155.5:
+        print(i)
+
+num_bins = 20
+bin_edges = np.linspace(min_score, max_score, num_bins+1)
+
+colors = LinearSegmentedColormap.from_list('', ['#6cac64', '#ccb45c'])
+
+plt.figure(figsize=(12, 6))
+n, bins, patches = plt.hist(scores, bins=bin_edges, edgecolor='black')
+
+for i, patch in enumerate(patches):
+    color = colors(i / len(patches))
+    patch.set_facecolor(color)
+
+plt.xlabel('Score', fontsize=12)
+plt.ylabel('Frequency', fontsize=12)
+plt.title('Distribution of Wordle Scores', fontsize=14)
+plt.grid(axis='y', alpha=0.3)
+
+# 添加颜色条
+sm = plt.cm.ScalarMappable(cmap=colors, norm=plt.Normalize(vmin=min_score, vmax=max_score))
+sm.set_array([])
+cbar = plt.colorbar(sm)
+cbar.set_label('Score Value', rotation=270, labelpad=15)
+
+plt.tight_layout()
+plt.show()
+
